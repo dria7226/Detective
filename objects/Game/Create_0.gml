a407d70b73ae4c4132cebf3bb80e03387fce1935
dragosh.iarovoi <dragosh.iarovoi@gmail.com>
2019-03-02 15:54:13 -0500

    Sync commit.

--- objects/Game/Create_0.gml
+++ objects/Game/Create_0.gml
@@ -2,3 +2,4 @@
 Log = file_text_open_write("log.txt");
 renderer_setup();
 game_setup();
+buffer_save(uniform_buffer, "uniform_buffer");
--- objects/Game/Create_0.gml.cpp
+++ objects/Game/Create_0.gml.cpp
@@ -3,3 +3,5 @@ Log = file_text_open_write("log.txt");
 renderer_setup();
 
 game_setup();
+
+buffer_save(uniform_buffer, "uniform_buffer");
